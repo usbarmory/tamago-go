674eae3ba7a7c072911605500bb16373d7d10302
Andrej Rosano <andrej.rosano@f-secure.com>
2019-10-23 21:10:27 +0200

    TamaGo initial commit
    Signed-off-by: Andrej Rosano <andrej.rosano@f-secure.com>

--- src/runtime/proc.go
+++ src/runtime/proc.go
@@ -166,7 +166,7 @@ func main() {
 	// Allow newproc to start new Ms.
 	mainStarted = true
 
-	if GOARCH != "wasm" { // no threads on wasm yet, so no sysmon
+	if GOARCH != "wasm" && GOOS != "tamago" { // no threads on wasm/tamago yet, so no sysmon
 		systemstack(func() {
 			newm(sysmon, nil, -1)
 		})
@@ -2153,7 +2153,8 @@ func newm1(mp *m) {
 //
 // The calling thread must itself be in a known-good state.
 func startTemplateThread() {
-	if GOARCH == "wasm" { // no threads on wasm yet
+	if GOARCH == "wasm" || // no threads on wasm yet
+	   GOOS == "tamago" {
 		return
 	}
 
@@ -3459,6 +3460,7 @@ func goexit0(gp *g) {
 	if isSystemGoroutine(gp, false) {
 		atomic.Xadd(&sched.ngsys, -1)
 	}
+
 	gp.m = nil
 	locked := gp.lockedm != 0
 	gp.lockedm = 0
@@ -3485,7 +3487,8 @@ func goexit0(gp *g) {
 
 	dropg()
 
-	if GOARCH == "wasm" { // no threads yet on wasm
+	if GOARCH == "wasm" || // no threads yet on wasm
+	   GOOS == "tamago" {
 		gfput(_p_, gp)
 		schedule() // never returns
 	}
@@ -4083,6 +4086,7 @@ func newproc1(fn *funcval, callergp *g, callerpc uintptr) *g {
 		casgstatus(newg, _Gidle, _Gdead)
 		allgadd(newg) // publishes with a g->status of Gdead so GC scanner doesn't look at uninitialized stack.
 	}
+
 	if newg.stack.hi == 0 {
 		throw("newproc1: newg missing stack")
 	}
@@ -4307,7 +4311,8 @@ func Breakpoint() {
 // or else the m might be different in this function than in the caller.
 //go:nosplit
 func dolockOSThread() {
-	if GOARCH == "wasm" {
+	if GOARCH == "wasm" ||
+	   GOOS == "tamago" {
 		return // no threads on wasm yet
 	}
 	_g_ := getg()
@@ -4358,7 +4363,8 @@ func lockOSThread() {
 // or else the m might be in different in this function than in the caller.
 //go:nosplit
 func dounlockOSThread() {
-	if GOARCH == "wasm" {
+	if GOARCH == "wasm" ||
+	   GOOS == "tamago" {
 		return // no threads on wasm yet
 	}
 	_g_ := getg()

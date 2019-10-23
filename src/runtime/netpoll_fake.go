f1b4f2bbf9a2d34546149f48de5b166812240d8d
Andrej Rosano <andrej.rosano@f-secure.com>
2019-10-23 21:10:27 +0200

    TamaGo initial commit
    Signed-off-by: Andrej Rosano <andrej.rosano@f-secure.com>

--- src/runtime/netpoll_fake.go
+++ src/runtime/netpoll_fake.go
@@ -5,8 +5,8 @@
 // Fake network poller for wasm/js.
 // Should never be used, because wasm/js network connections do not honor "SetNonblock".
 
-//go:build js && wasm
-// +build js,wasm
+//go:build (js && wasm) || tamago
+// +build js,wasm tamago
 
 package runtime
 

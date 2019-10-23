fb8347203e816202ec3fc2ea1435a7cd2a2ce659
Andrej Rosano <andrej.rosano@f-secure.com>
2019-10-23 21:10:27 +0200

    TamaGo initial commit
    Signed-off-by: Andrej Rosano <andrej.rosano@f-secure.com>

--- src/cmd/dist/build.go
+++ src/cmd/dist/build.go
@@ -91,6 +91,7 @@ var okgoos = []string{
 	"plan9",
 	"windows",
 	"aix",
+	"tamago",
 }
 
 // find reports the first index of p in l[0:n], or else -1.
@@ -1558,6 +1559,7 @@ var cgoEnabled = map[string]bool{
 	"windows/386":     true,
 	"windows/amd64":   true,
 	"windows/arm":     false,
+	"tamago/arm":      true,
 }
 
 // List of platforms which are supported but not complete yet. These get

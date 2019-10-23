b7a1b753cbd9521981d23570120d9a7ce8fed491
Andrej Rosano <andrej.rosano@f-secure.com>
2019-10-23 21:10:27 +0200

    TamaGo initial commit
    Signed-off-by: Andrej Rosano <andrej.rosano@f-secure.com>

---
+++ src/os/user/lookup_tamago.go
@@ -0,0 +1,29 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package user
+
+import (
+	"errors"
+)
+
+func init() {
+	groupImplemented = false
+}
+
+func lookupUser(username string) (*User, error) {
+	return nil, errors.New("unsupported")
+}
+
+func lookupUserId(uid string) (*User, error) {
+	return nil, errors.New("unsupported")
+}
+
+func lookupGroup(groupname string) (*Group, error) {
+	return nil, errors.New("unsupported")
+}
+
+func lookupGroupId(string) (*Group, error) {
+	return nil, errors.New("unsupported")
+}

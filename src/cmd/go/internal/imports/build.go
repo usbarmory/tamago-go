b9769fd9383dc0daa21d2e1290d35438cb9f882c
Andrej Rosano <andrej.rosano@f-secure.com>
2019-10-23 21:10:27 +0200

    TamaGo initial commit
    Signed-off-by: Andrej Rosano <andrej.rosano@f-secure.com>

--- src/cmd/go/internal/imports/build.go
+++ src/cmd/go/internal/imports/build.go
@@ -217,6 +217,7 @@ var KnownOS = map[string]bool{
 	"solaris":   true,
 	"windows":   true,
 	"zos":       true,
+	"tamago":    true,
 }
 
 var KnownArch = map[string]bool{
--- src/cmd/link/internal/amd64/obj.go
+++ src/cmd/link/internal/amd64/obj.go
@@ -98,6 +98,7 @@ func archinit(ctxt *ld.Link) {
 		}
 
 	case objabi.Hlinux, /* elf64 executable */
+		objabi.Htamago,    /* tamago */
 		objabi.Hfreebsd,   /* freebsd */
 		objabi.Hnetbsd,    /* netbsd */
 		objabi.Hopenbsd,   /* openbsd */

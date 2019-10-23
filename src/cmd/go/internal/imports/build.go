78ff65e0228c92ba65f4a74e043393d181e26bc4
Andrej Rosano <andrej.rosano@f-secure.com>
2019-10-23 21:10:27 +0200

    TamaGo initial commit
    Signed-off-by: Andrej Rosano <andrej.rosano@f-secure.com>

--- src/cmd/go/internal/imports/build.go
+++ src/cmd/go/internal/imports/build.go
@@ -326,6 +326,7 @@ var KnownOS = map[string]bool{
 	"wasip1":    true,
 	"windows":   true,
 	"zos":       true,
+	"tamago":    true,
 }
 
 // unixOS is the set of GOOS values matched by the "unix" build tag.
--- src/runtime/proc.go
+++ src/runtime/proc.go
@@ -167,7 +167,7 @@ func main() {
 	// Allow newproc to start new Ms.
 	mainStarted = true
 
-	if GOARCH != "wasm" { // no threads on wasm yet, so no sysmon
+	if GOARCH != "wasm" && GOOS != "tamago" { // no threads on wasm/tamago yet, so no sysmon
 		systemstack(func() {
 			newm(sysmon, nil, -1)
 		})
@@ -2707,7 +2707,8 @@ func newm1(mp *m) {
 //
 // The calling thread must itself be in a known-good state.
 func startTemplateThread() {
-	if GOARCH == "wasm" { // no threads on wasm yet
+	if GOARCH == "wasm" || // no threads on wasm yet
+	   GOOS == "tamago" {
 		return
 	}
 
@@ -4190,6 +4191,7 @@ func gdestroy(gp *g) {
 	if isSystemGoroutine(gp, false) {
 		sched.ngsys.Add(-1)
 	}
+
 	gp.m = nil
 	locked := gp.lockedm != 0
 	gp.lockedm = 0
@@ -4216,7 +4218,8 @@ func gdestroy(gp *g) {
 
 	dropg()
 
-	if GOARCH == "wasm" { // no threads yet on wasm
+	if GOARCH == "wasm" || // no threads yet on wasm
+	   GOOS == "tamago" {
 		gfput(pp, gp)
 		return
 	}
@@ -4902,6 +4905,7 @@ func newproc1(fn *funcval, callergp *g, callerpc uintptr) *g {
 		casgstatus(newg, _Gidle, _Gdead)
 		allgadd(newg) // publishes with a g->status of Gdead so GC scanner doesn't look at uninitialized stack.
 	}
+
 	if newg.stack.hi == 0 {
 		throw("newproc1: newg missing stack")
 	}
@@ -5161,7 +5165,8 @@ func Breakpoint() {
 //
 //go:nosplit
 func dolockOSThread() {
-	if GOARCH == "wasm" {
+	if GOARCH == "wasm" ||
+	   GOOS == "tamago" {
 		return // no threads on wasm yet
 	}
 	gp := getg()
@@ -5213,7 +5218,8 @@ func lockOSThread() {
 //
 //go:nosplit
 func dounlockOSThread() {
-	if GOARCH == "wasm" {
+	if GOARCH == "wasm" ||
+	   GOOS == "tamago" {
 		return // no threads on wasm yet
 	}
 	gp := getg()

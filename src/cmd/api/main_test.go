d68efe1c018adf6bb475cd3f6cd44daaf79b29a1
Andrea Barisani <andrea@inversepath.com>
2020-01-09 10:18:32 +0100

    ensure that all.bash test pass with tamago patches

--- src/cmd/api/main_test.go
+++ src/cmd/api/main_test.go
@@ -82,8 +82,6 @@ var contexts = []*build.Context{
 	{GOOS: "openbsd", GOARCH: "386"},
 	{GOOS: "openbsd", GOARCH: "amd64", CgoEnabled: true},
 	{GOOS: "openbsd", GOARCH: "amd64"},
-	{GOOS: "tamago", GOARCH: "arm", CgoEnabled: true},
-	{GOOS: "tamago", GOARCH: "arm"},
 }
 
 func contextName(c *build.Context) string {

440847db0674642450b668ef8fe742dad0f14fff
Andrea Barisani <andrea@inversepath.com>
2020-11-16 10:05:02 +0100

    allow CGO_ENABLED=1 for GOOS=tamago (WiP)

--- src/cmd/go/internal/work/exec.go
+++ src/cmd/go/internal/work/exec.go
@@ -2278,6 +2278,8 @@ func (b *Builder) compilerCmd(compiler []string, incdir, workdir string) []strin
 		switch cfg.Goos {
 		case "windows":
 			a = append(a, "-mthreads")
+		case "tamago":
+			a = append(a, []string{"-ffreestanding", "-specs=nosys.specs"}...)
 		default:
 			a = append(a, "-pthread")
 		}

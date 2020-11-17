247b87998acdd9d4880fa6193a5a238352e0a13b
Andrea Barisani <andrea@inversepath.com>
2020-11-17 20:38:01 +0100

    Revert "allow CGO_ENABLED=1 for GOOS=tamago (WiP)"
    This reverts commit 161eb63d67e3d79afbe75c650f858fe3a98a307b.

--- src/cmd/go/internal/work/exec.go
+++ src/cmd/go/internal/work/exec.go
@@ -2795,8 +2795,6 @@ func (b *Builder) compilerCmd(compiler []string, incdir, workdir string) []strin
 		switch cfg.Goos {
 		case "windows":
 			a = append(a, "-mthreads")
-		case "tamago":
-			a = append(a, []string{"-ffreestanding", "-specs=nosys.specs"}...)
 		default:
 			a = append(a, "-pthread")
 		}

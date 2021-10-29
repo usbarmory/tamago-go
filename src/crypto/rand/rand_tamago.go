f8f7517eb73f3846c0d1a3ef54982fa26838a48e
Andrea Barisani <andrea@inversepath.com>
2021-10-29 09:43:52 +0200

    align build tags to new syntax

--- src/crypto/rand/rand_tamago.go
+++ src/crypto/rand/rand_tamago.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build tamago
 // +build tamago
 
 package rand
@@ -16,7 +17,7 @@ func init() {
 }
 
 type reader struct {
-	mu   sync.Mutex
+	mu sync.Mutex
 }
 
 func (r *reader) Read(b []byte) (int, error) {

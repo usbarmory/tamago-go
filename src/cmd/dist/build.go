208c17b98e50b473a44946f0258fc657ad50bd53
Andrea Barisani <andrea@inversepath.com>
2022-06-24 10:50:24 +0200

    initial support for tamago/riscv64

--- src/cmd/dist/build.go
+++ src/cmd/dist/build.go
@@ -1742,6 +1742,7 @@ var cgoEnabled = map[string]bool{
 	"windows/arm":     false,
 	"windows/arm64":   true,
 	"tamago/arm":      true,
+	"tamago/riscv64":  true,
 }
 
 // List of platforms that are marked as broken ports.

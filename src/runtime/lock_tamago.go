24a17de6a78d331a3e51b9126ee497fae59f7383
Andrea Barisani <andrea@inversepath.com>
2022-06-24 10:50:24 +0200

    initial support for tamago/riscv64

--- src/runtime/lock_tamago.go
+++ src/runtime/lock_tamago.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build tamago && arm
-// +build tamago,arm
+//go:build tamago
+// +build tamago
 
 package runtime
 

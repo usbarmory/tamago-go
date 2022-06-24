1910ec4125689a291c704f1f7f5c58991d2da82b
Andrea Barisani <andrea@inversepath.com>
2022-06-24 10:50:24 +0200

    initial support for tamago/riscv64

--- src/runtime/mpagealloc_32bit.go
+++ src/runtime/mpagealloc_32bit.go
@@ -2,12 +2,16 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build 386 || arm || mips || mipsle || wasm
+//go:build 386 || arm || mips || mipsle || wasm || tamago
 
-// wasm is a treated as a 32-bit architecture for the purposes of the page
+// wasm is treated as a 32-bit architecture for the purposes of the page
 // allocator, even though it has 64-bit pointers. This is because any wasm
 // pointer always has its top 32 bits as zero, so the effective heap address
 // space is only 2^32 bytes in size (see heapAddrBits).
+
+// tamago is treated as a 32-bit architecture for the purposes of the page
+// allocator, even though it has 64-bit pointers. This is to reduce heap
+// allocation requirements.
 
 package runtime
 

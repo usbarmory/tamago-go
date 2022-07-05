bfd30d49dfe333c8cfde97bf9e112226609d7b2e
Andrea Barisani <andrea@inversepath.com>
2022-07-05 11:01:28 +0200

    avoid RDTIME on tamago/riscv64

--- src/runtime/os_tamago_riscv64.go
+++ src/runtime/os_tamago_riscv64.go
@@ -88,6 +88,13 @@ func os_sigpipe() {
 }
 
 //go:nosplit
+func cputicks() int64 {
+	// Currently cputicks() is used in blocking profiler and to seed runtime·fastrand().
+	// runtime·nanotime() is a poor approximation of CPU ticks that is enough for the profiler.
+	return nanotime()
+}
+
+//go:nosplit
 func crash() {
 	*(*int32)(nil) = 0
 }

d85fb9cff7f140d1309e51fbd81e76c6a35e4021
Andrea Barisani <andrea@inversepath.com>
2023-06-23 16:49:00 +0200

    add native networking to GOOS=tamago

--- src/os/file.go
+++ src/os/file.go
@@ -430,6 +430,9 @@ func UserCacheDir() (string, error) {
 		}
 		dir += "/lib/cache"
 
+	case "tamago":
+		dir = "/"
+
 	default: // Unix
 		dir = Getenv("XDG_CACHE_HOME")
 		if dir == "" {
@@ -480,6 +483,9 @@ func UserConfigDir() (string, error) {
 			return "", errors.New("$home is not defined")
 		}
 		dir += "/lib"
+
+	case "tamago":
+		dir = "/"
 
 	default: // Unix
 		dir = Getenv("XDG_CONFIG_HOME")

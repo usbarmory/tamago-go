d936a215e4896538139efff5cbab610af45ad90f
Andrea Barisani <andrea@inversepath.com>
2024-08-28 16:20:22 +0200

    improve GOOS=tamago test coverage

--- src/runtime/gc_test.go
+++ src/runtime/gc_test.go
@@ -164,7 +164,7 @@ func TestHugeGCInfo(t *testing.T) {
 }
 
 func TestPeriodicGC(t *testing.T) {
-	if runtime.GOARCH == "wasm" {
+	if runtime.GOARCH == "wasm" || runtime.GOOS == "tamago" {
 		t.Skip("no sysmon on wasm yet")
 	}
 

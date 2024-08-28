e71ff011e80a555ed3e6171378c421c6d4c311d6
Andrea Barisani <andrea@inversepath.com>
2024-08-28 16:20:22 +0200

    improve GOOS=tamago test coverage

--- src/net/main_tamago_test.go
+++ src/net/main_tamago_test.go
@@ -2,12 +2,25 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build tamago
-
 package net
 
-func installTestHooks() {}
+import (
+	"context"
+)
+
+func installTestHooks() {
+	SocketFunc = func(context.Context, string, int, int, Addr, Addr) (i interface{}, err error) {
+		return nil, nil
+	}
+}
 
 func uninstallTestHooks() {}
 
+// forceCloseSockets must be called only from TestMain.
 func forceCloseSockets() {}
+
+func enableSocketConnect() {}
+
+func disableSocketConnect(network string) {}
+
+func isDeadlineExceeded(err error) bool { return false }

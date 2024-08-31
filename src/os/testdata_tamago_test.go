a80f7fc65962ea369a6e29061746a0f0d72c78b4
Andrea Barisani <andrea@inversepath.com>
2024-09-01 00:29:32 +0200

    improve GOOS=tamago test coverage

--- src/os/testdata_tamago_test.go
+++ src/os/testdata_tamago_test.go
@@ -1,10 +1,23 @@
 //go:build tamago
-package os
+package os_test
 
 import (
 	"embed"
+	"os"
+	"syscall"
 )
+
+//go:embed *.go exec/*
+var src embed.FS
 
 //go:embed testdata/*
 var testdata embed.FS
-func init() { CopyFS(".", testdata) }
+
+func init() {
+	// cwd in TestDirFSRootDir must not be empty
+	os.MkdirAll("./1", 0777)
+	syscall.Chdir("./1")
+
+	os.CopyFS(".", src)
+	os.CopyFS(".", testdata)
+}

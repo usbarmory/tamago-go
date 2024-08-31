e14c838c77bfa438118b31ba9ae8f24f3bde2602
Andrea Barisani <andrea@inversepath.com>
2024-08-31 18:10:04 +0200

    improve GOOS=tamago test coverage

---
+++ src/cmd/asm/internal/asm/testdata_tamago_test.go
@@ -0,0 +1,35 @@
+//go:build tamago
+package asm
+
+import (
+	"embed"
+	"os"
+	"syscall"
+)
+
+//go:embed testdata/*
+var testdata embed.FS
+
+var textflag = `
+#define NOPROF	1
+#define DUPOK	2
+#define NOSPLIT	4
+#define RODATA	8
+#define NOPTR	16
+#define WRAPPER 32
+#define NEEDCTXT 64
+#define TLSBSS	256
+#define NOFRAME 512
+#define REFLECTMETHOD 1024
+#define TOPFRAME 2048
+#define ABIWRAPPER 4096
+`
+
+func init() {
+	// tests look for path ../../../../runtime/textflag.h
+	os.MkdirAll("runtime", 0777)
+	os.WriteFile("runtime/textflag.h", []byte(textflag), 0600)
+	os.MkdirAll("./1/2/3/4", 0777)
+	syscall.Chdir("./1/2/3/4")
+	os.CopyFS(".", testdata)
+}

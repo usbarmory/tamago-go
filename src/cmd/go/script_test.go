b9fe7cbf04562b59d2a957fde86ae6eeb4e871a0
Andrea Barisani <andrea@inversepath.com>
2024-08-31 10:52:39 +0200

    typo fix

--- src/cmd/go/script_test.go
+++ src/cmd/go/script_test.go
@@ -421,5 +421,5 @@ const disabledOnPlatform = false ||
 	// These platforms fundamentally can't be supported:
 	runtime.GOOS == "js" || // #60971
 	runtime.GOOS == "wasip1" || // #60971
-	runtime.GOOS == "plan9" // https://github.com/golang/go/issues/57540#issuecomment-1470766639
+	runtime.GOOS == "plan9" || // https://github.com/golang/go/issues/57540#issuecomment-1470766639
 	runtime.GOOS == "tamago"

90f858cfe487c827c908706e2cc6b19ed4a3226a
Andrea Barisani <andrea@inversepath.com>
2024-09-10 15:23:25 +0200

    improve GOOS=tamago test coverage

--- src/cmd/internal/testdir/testdir_test.go
+++ src/cmd/internal/testdir/testdir_test.go
@@ -116,10 +116,6 @@ func Test(t *testing.T) {
 	goExperiment = env.GOEXPERIMENT
 	goDebug = env.GODEBUG
 
-	if goos == "tamago" {
-		t.Skipf("skipping: unsupported")
-	}
-
 	common := testCommon{
 		gorootTestDir: filepath.Join(testenv.GOROOT(t), "test"),
 		runoutputGate: make(chan bool, *runoutputLimit),

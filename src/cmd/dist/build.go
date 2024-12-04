1166564969508a45731326c88cfd10750dcb7466
Andrea Barisani <andrea@inversepath.com>
2024-12-04 12:33:06 +0100

    add tamago/amd64 support (#11)
    * remove unused constant
    
    * WiP tamago/amd64 support
    
    * WiP tamago/amd64 support
    
    * WiP tamago/amd64 support
    
    * tidying
    
    * ensure entry point consistency with other archs
    
    * tidying
    
    * move processor initialization outside tamago-go
    
    * update github action
    
    * tidying
    
    * add GetG to tamago/amd64
    
    * unify GOOS=tamago amd64/arm/riscv64 runtime support

--- src/cmd/dist/build.go
+++ src/cmd/dist/build.go
@@ -1813,6 +1813,7 @@ var cgoEnabled = map[string]bool{
 	"plan9/amd64":     false,
 	"plan9/arm":       false,
 	"solaris/amd64":   true,
+	"tamago/amd64":    false,
 	"tamago/arm":      false,
 	"tamago/riscv64":  false,
 	"windows/386":     true,

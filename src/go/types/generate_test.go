12eb0a3cc20b9ab1e0f03b1075381c391c5e0001
Andrea Barisani <andrea@inversepath.com>
2025-02-12 09:54:44 +0100

    align GOOS=tamago support to go1.24.0

--- src/go/types/generate_test.go
+++ src/go/types/generate_test.go
@@ -53,11 +53,7 @@ func TestGenerate(t *testing.T) {
 }
 
 func generate(t *testing.T, filename string, write bool) {
-	if !testenv.HasSrc() {
-		// Tests run in a limited file system and we do not
-		// provide access to every source file.
-		t.Skipf("skipping on %s/%s, missing full GOROOT", runtime.GOOS, runtime.GOARCH)
-	}
+	testenv.MustHaveSource(t)
 
 	// parse src (cmd/compile/internal/types2)
 	srcFilename := filepath.FromSlash(runtime.GOROOT() + srcDir + filename)

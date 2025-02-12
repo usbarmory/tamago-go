1a8544eb05a89fc4ba751d0ab146af0d606e07f1
Andrea Barisani <andrea@inversepath.com>
2025-02-12 11:46:42 +0100

    align GOOS=tamago support to go1.24.0

---
+++ src/crypto/internal/fips140/rsa/testdata_tamago_test.go
@@ -0,0 +1,15 @@
+// Copyright 2025 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build tamago
+package rsa
+
+import (
+	"embed"
+	"os"
+)
+
+//go:embed testdata/*
+var testdata embed.FS
+func init() { os.CopyFS(".", testdata) }

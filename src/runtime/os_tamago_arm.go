d4cd9c89827fec8347f9c5af6e2bf276a3118b60
Andrea Barisani <andrea@inversepath.com>
2025-03-12 22:54:29 +0100

    add runtime.DataRegion for GOOS=tamago

--- src/runtime/os_tamago_arm.go
+++ src/runtime/os_tamago_arm.go
@@ -33,6 +33,12 @@ func TextRegion() (start uint32, end uint32) {
 	return uint32(firstmoduledata.text), uint32(firstmoduledata.etext)
 }
 
+// DataRegion returns the start and end addresses of the physical RAM
+// containing the Go runtime global symbols.
+func DataRegion() (start uint32, end uint32) {
+	return uint32(firstmoduledata.data), uint32(firstmoduledata.enoptrbss)
+}
+
 func checkgoarm() {
 	if goarm < 5 || goarm > 7 {
 		print("runtime: tamago requires ARMv5 through ARMv7. Recompile using GOARM=5, GOARM=6 or GOARM=7.\n")

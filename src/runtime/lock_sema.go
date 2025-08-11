adb1b4ca74e8ff576a0073b21b38357c5752e2b9
Andrea Barisani <andrea@inversepath.com>
2025-08-11 16:35:32 +0200

    SMP support for GOOS=tamago (#12)
    * migrate GOOS=tamago implementation to multi-threading
    * improve runtime.WakeG/runtime.Wake implementation
    * add multithreaded testing support for GOOS=tamago
    * do not force/restrict GOMAXPROCS when testing GOOS=tamago
    * multi-threaded distribution tests are no longer skipped on GOOS=tamago

--- src/runtime/lock_sema.go
+++ src/runtime/lock_sema.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build aix || darwin || netbsd || openbsd || plan9 || solaris || windows
+//go:build aix || darwin || netbsd || openbsd || plan9 || solaris || windows || tamago
 
 package runtime
 
@@ -177,9 +177,3 @@ func notetsleepg(n *note, ns int64) bool {
 	exitsyscall()
 	return ok
 }
-
-func beforeIdle(int64, int64) (*g, bool) {
-	return nil, false
-}
-
-func checkTimeouts() {}

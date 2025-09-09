4ab99a6d2998d6e7c413e130e1efd4b00b2b165f
Andrea Barisani <andrea@inversepath.com>
2025-09-09 18:45:45 +0200

    add tamago/arm64 support

--- src/cmd/dist/build.go
+++ src/cmd/dist/build.go
@@ -1831,6 +1831,7 @@ var cgoEnabled = map[string]bool{
 	"solaris/amd64":   true,
 	"tamago/amd64":    false,
 	"tamago/arm":      false,
+	"tamago/arm64":    false,
 	"tamago/riscv64":  false,
 	"windows/386":     true,
 	"windows/amd64":   true,

a8364681254a499c83c08eb2d1b012135956770e
Andrea Barisani <andrea@inversepath.com>
2025-09-09 18:45:45 +0200

    add tamago/arm64 support

--- src/testing/testing_tamago.go
+++ src/testing/testing_tamago.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build tamago && (amd64 || arm || riscv64)
+//go:build tamago && (amd64 || arm || arm64 || riscv64)
 
 package testing
 

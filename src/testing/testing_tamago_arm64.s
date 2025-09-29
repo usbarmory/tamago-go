9e12de39cd861890279dda747b92bbbd6f3c4f73
Andrea Barisani <andrea@inversepath.com>
2025-09-29 23:40:28 +0200

    require user_linux build tag to distinguish test runs from testing import

--- src/testing/testing_tamago_arm64.s
+++ src/testing/testing_tamago_arm64.s
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build tamago && arm64
+//go:build tamago && arm64 && user_linux
 
 #include "go_asm.h"
 #include "textflag.h"

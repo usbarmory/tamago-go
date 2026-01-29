604dec95320632c4d0c95807289ca73336fee01f
Andrea Barisani <andrea@inversepath.com>
2026-01-29 11:57:59 +0100

    cleanup

--- src/runtime/os_tamago_arm.go
+++ src/runtime/os_tamago_arm.go
@@ -13,24 +13,6 @@ package runtime
 // definition in sys_tamago_arm.s for details.
 func CallOnG0()
 
-// MemRegion returns the start and end addresses of the physical RAM assigned
-// to the Go runtime.
-func MemRegion() (start uint32, end uint32) {
-	return ramStart, ramStart + ramSize
-}
-
-// TextRegion returns the start and end addresses of the physical RAM
-// containing the Go runtime executable instructions.
-func TextRegion() (start uint32, end uint32) {
-	return uint32(firstmoduledata.text), uint32(firstmoduledata.etext)
-}
-
-// DataRegion returns the start and end addresses of the physical RAM
-// containing the Go runtime global symbols.
-func DataRegion() (start uint32, end uint32) {
-	return uint32(firstmoduledata.data), uint32(firstmoduledata.enoptrbss)
-}
-
 func checkgoarm() {
 	if goarm < 5 || goarm > 7 {
 		print("runtime: tamago requires ARMv5 through ARMv7. Recompile using GOARM=5, GOARM=6 or GOARM=7.\n")

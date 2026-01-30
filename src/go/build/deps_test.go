425fbbc8e5c10a09ca44a6fe92c51fe57310f768
Andrea Barisani <andrea@inversepath.com>
2026-01-30 12:49:41 +0100

    fix dependency test

--- src/go/build/deps_test.go
+++ src/go/build/deps_test.go
@@ -74,6 +74,9 @@ var depsRules = `
 
 	internal/cpu, internal/strconv < simd/archsimd;
 
+	unsafe
+	< runtime/goos;
+
 	# RUNTIME is the core runtime group of packages, all of them very light-weight.
 	internal/abi,
 	internal/chacha8rand,
@@ -90,6 +93,7 @@ var depsRules = `
 	math/bits,
 	structs,
 	simd/archsimd
+	runtime/goos,
 	< internal/bytealg
 	< internal/stringslite
 	< internal/unsafeheader

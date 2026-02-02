e6fa2a2a305ed3e17422d3b9929ee0236e532d6a
Andrea Barisani <andrea@inversepath.com>
2026-02-02 12:16:46 +0100

    remove runtime.testBinary need

--- src/cmd/go/internal/load/test.go
+++ src/cmd/go/internal/load/test.go
@@ -302,7 +302,7 @@ func TestPackagesAndErrors(ld *modload.Loader, ctx context.Context, done func(),
 	}
 
 	// Arrange for testing.Testing to report true.
-	ldflags := append(p.Internal.Ldflags, "-X", "testing.testBinary=1", "-X", "runtime.testBinary=1")
+	ldflags := append(p.Internal.Ldflags, "-X", "testing.testBinary=1")
 	gccgoflags := append(p.Internal.Gccgoflags, "-Wl,--defsym,testing.gccgoTestBinary=1")
 
 	// Build main package.

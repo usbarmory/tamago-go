0798512b677f34d5b694ca3481b9a5aa6c5fe307
Andrea Barisani <andrea@inversepath.com>
2026-02-11 00:55:01 +0100

    move GOOSPKG implementation from module path to repository root path

--- src/cmd/go/internal/goos/goos.go
+++ src/cmd/go/internal/goos/goos.go
@@ -6,20 +6,18 @@
 // proposal #73608).
 //
 // The GOOSPKG build setting controls which copy of the runtime/goos source
-// code to use. The default is obviously GOROOT/src/runtime/goos, but different
-// implementations can be substituted into the build instead.
+// code to use. The default is obviously GOROOT/src/runtime/goos, but a
+// different implementation can be substituted into the build instead.
 //
 // This package provides the logic needed by the rest of the go command
 // to implement the overlay.
 //
-// [Init] must be called to initialize the GOOSPKG logic. It may fail and call
-// base.Fatalf.
-//
 // When GOOSPKG is empty GOROOT/src/runtime/goos is imported as expected to
 // resolve [runtime/goos].
 //
-// When GOOSPKG is set it defines a module, or filesystem path, as source
-// location for [runtime/goos].
+// When GOOSPKG is set it defines a module repository root path to be used as
+// alias for [runtime/goos], the implementation must live under module
+// subdirectory "goos".
 //
 // ResolveImport is called to resolve the [runtime/goos] import, in a manner
 // similar to fips140 snapshot logic (see GOROOT/src/cmd/go/internal/fips140).
@@ -28,120 +26,46 @@ package goos
 import (
 	"context"
 	"os"
-	"path"
 	"path/filepath"
-	"strings"
 
 	"cmd/go/internal/base"
 	"cmd/go/internal/cfg"
-	"cmd/go/internal/modfetch"
+	"cmd/go/internal/modload"
 	"cmd/go/internal/str"
-
-	"golang.org/x/mod/module"
 )
 
-// Init initializes the GOOS=tamago settings.
-// It must be called before using any other functions in this package.
-// If initialization fails, Init calls base.Fatalf.
-func Init() {
-	if initDone {
-		return
-	}
-	initDone = true
-	initVersion()
-	initDir()
-
-	if cfg.Goos != "tamago" || cfg.GOOSPKG != "" {
-		return
-	}
-
-	// fallback to Linux userspace goos defined in GOROOT/src/runtime/goos
-	if os.Getenv("GOHOSTOS") == "linux" && (cfg.Goarch == "amd64" || cfg.Goarch == "arm" || cfg.Goarch == "arm64" || cfg.Goarch == "riscv64") {
-		return
-	}
-
-	base.Fatalf("go: cannot use GOOS %s with empty GOOSPKG on %s/%s", cfg.Goos, os.Getenv("GOHOSTOS"), cfg.Goarch)
-}
-
-var initDone bool
-
-// checkInit panics if Init has not been called.
-func checkInit() {
-	if !initDone {
-		panic("goos: not initialized")
-	}
-}
-
-var name string
-var version string
-
-func initVersion() {
-	if cfg.GOOSPKG == "" {
-		return
-	}
-
-	n, v, found := strings.Cut(cfg.GOOSPKG, "@")
-
-	if found {
-		name = n
-		version = v
-		return
-	}
-
-	if _, err := os.Stat(cfg.GOOSPKG); err != nil {
-		base.Fatalf("go: unknown GOOSPKG %q, %v", cfg.GOOSPKG, err)
-	}
-
-	dir = filepath.Join(cfg.GOOSPKG, "goos")
-
-	return
-}
-
-// Dir reports the directory containing the runtime/goos source code.
-// If GOOSPKG is empty, Dir returns GOROOT/src/runtime/goos.
-// Otherwise Dir ensures that the snapshot has been unpacked into the
-// module cache and then returns the directory in the module cache
-// corresponding to the runtime/goos directory.
-func Dir() string {
-	checkInit()
-	return dir
-}
-
-var dir string
-
-func initDir() {
-	if dir != "" {
-		return
-	}
-
-	if version == "" {
-		dir = filepath.Join(cfg.GOROOT, "src/runtime/goos")
-		return
-	}
-
-	mod := module.Version{Path: name, Version: version}
-	mdir, err := modfetch.NewFetcher().Download(context.Background(), mod)
-	if err != nil {
-		base.Fatalf("go: downloading GOOSPKG=%q: %v", cfg.GOOSPKG, err)
-	}
-	dir = filepath.Join(mdir, "goos")
-}
+const goos = "runtime/goos"
 
 // ResolveImport resolves the import path imp.
-func ResolveImport(imp string) (newPath, dir string, ok bool) {
-	checkInit()
-	const goos = "runtime/goos"
-	if !str.HasPathPrefix(imp, goos) {
+func ResolveImport(loaderstate *modload.State, imp string) (newPath, dir string, ok bool) {
+	if !str.HasPathPrefix(imp, goos) || cfg.Goos != "tamago" {
 		return "", "", false
 	}
-	goosv := path.Join(goos, version)
-	var sub string
-	if str.HasPathPrefix(imp, goosv) {
-		sub = "." + imp[len(goosv):]
+
+	if cfg.GOOSPKG != "" {
+		r, err := modload.ListModules(loaderstate, context.Background(), []string{cfg.GOOSPKG}, 0, "")
+
+		if err != nil {
+			base.Fatalf("go: GOOSPKG=%q not found in module list: %v", cfg.GOOSPKG, err)
+		}
+
+		if len(r) > 0 && r[0].Error == nil {
+			dir = r[0].Dir
+		}
+
+		if len(dir) == 0 {
+			base.Fatalf("go: GOOSPKG=%q not found in module list", cfg.GOOSPKG)
+		}
+
+		dir = filepath.Join(dir, "goos")
 	} else {
-		sub = "." + imp[len(goos):]
+		// fallback to Linux userspace goos defined in GOROOT/src/runtime/goos
+		if os.Getenv("GOHOSTOS") == "linux" && (cfg.Goarch == "amd64" || cfg.Goarch == "arm" || cfg.Goarch == "arm64" || cfg.Goarch == "riscv64") {
+			dir = filepath.Join(cfg.GOROOT, "src/runtime/goos")
+		} else {
+			base.Fatalf("go: GOOS %s unsupported without external GOOSPKG on %s/%s", cfg.Goos, os.Getenv("GOHOSTOS"), cfg.Goarch)
+		}
 	}
-	newPath = path.Join(goos, sub)
-	dir = filepath.Join(Dir(), sub)
-	return newPath, dir, true
+
+	return goos, dir, true
 }
--- src/cmd/go/internal/load/pkg.go
+++ src/cmd/go/internal/load/pkg.go
@@ -404,7 +404,7 @@ func (p *Package) copyBuild(opts PackageOpts, pp *build.Package) {
 	p.BinaryOnly = pp.BinaryOnly
 
 	// TODO? Target
-	p.Goroot = pp.Goroot || fips140.Snapshot() && str.HasFilePathPrefix(p.Dir, fips140.Dir()) || str.HasFilePathPrefix(p.Dir, goos.Dir())
+	p.Goroot = pp.Goroot || fips140.Snapshot() && str.HasFilePathPrefix(p.Dir, fips140.Dir())
 	p.Standard = p.Goroot && p.ImportPath != "" && search.IsStandardImportPath(p.ImportPath)
 	p.GoFiles = pp.GoFiles
 	p.CgoFiles = pp.CgoFiles
@@ -902,7 +902,7 @@ func loadPackageData(ld *modload.Loader, ctx context.Context, path, parentPath, 
 		if newPath, dir, ok := fips140.ResolveImport(path); ok {
 			r.path = newPath
 			r.dir = dir
-		} else if newPath, dir, ok := goos.ResolveImport(path); ok {
+		} else if newPath, dir, ok := goos.ResolveImport(loaderstate, path); ok {
 			r.path = newPath
 			r.dir = dir
 		} else if cfg.ModulesEnabled {

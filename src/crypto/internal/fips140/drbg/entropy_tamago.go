896ec1a6caee3ece3519f67ca9bf48f1a2981e31
Andrea Barisani <andrea@inversepath.com>
2026-02-12 12:28:13 +0100

    remove 32MB symbol from .noptrbss to reduce size of GOOS=tamago binary unikernels, without touching FIPS certified entropy package

--- src/crypto/internal/fips140/drbg/entropy_tamago.go
+++ src/crypto/internal/fips140/drbg/entropy_tamago.go
@@ -1,16 +1,8 @@
-// Copyright 2026 The Go Authors. All rights reserved.
+// Copyright 2024 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !wasm && !tamago
-
-// This file contains reading from from entropy sources in FIPS-140
-// mode. It uses a scratch buffer in the BSS section (see below),
-// which usually doesn't cost much, except on Wasm, due to the way
-// the linear memory works. FIPS-140 mode is not supported on Wasm,
-// so we just use a build tag to exclude it. (Could also exclude other
-// platforms that does not support FIPS-140 mode, but as the BSS
-// variable doesn't cost much, don't bother.)
+//go:build tamago
 
 package drbg
 
@@ -28,14 +20,18 @@ import (
 // cost of zeroing a new allocation every time. The entropy source accesses it
 // using atomics (and doesn't care about its contents).
 //
-// It should end up in the .noptrbss section, and become backed by physical pages
-// at first use. This ensures that programs that do not use the FIPS 140-3 module
-// do not incur any memory use or initialization penalties.
-var memory entropy.ScratchBuffer
+// In GOOS=tamago it is dynamically allocated at first use to prevent
+// allocation in the .noptrbss section, which would cause overhead on ELF to
+// binary conversions.
+var getMemory = sync.OnceValue(func() *entropy.ScratchBuffer {
+	return new(entropy.ScratchBuffer)
+})
 
 func getEntropy() *[SeedSize]byte {
+	memory := getMemory()
+
 	var retries int
-	seed, err := entropy.Seed(&memory)
+	seed, err := entropy.Seed(memory)
 	for err != nil {
 		// The CPU jitter-based SP 800-90B entropy source has a non-negligible
 		// chance of failing the startup health tests.
@@ -46,7 +42,7 @@ func getEntropy() *[SeedSize]byte {
 		if retries++; retries > 100 {
 			panic("fips140/drbg: failed to obtain initial entropy")
 		}
-		seed, err = entropy.Seed(&memory)
+		seed, err = entropy.Seed(memory)
 	}
 	return &seed
 }
